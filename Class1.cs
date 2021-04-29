fb826d6ca9d302a5abea863f2f23fcda0b3caff5
Petter Ekrann <petter.ekrann@visavi.com>
2021-04-29 15:28:18 +0200

    Added prod network c# class

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Deferment
+{
+    
+    public class ProductionNetworkNode
+    {
+        public Guid Id;
+        public string Name;
+        public string Description;
+        public string Type;
+
+        public List<ConnectionInfo> Connections;
+        public List<ProductionLimits> Production;
+        public List<ExtendedInfo> ExtendedeInfo;
+
+    }
+
+    public class ExtendedInfo
+    {
+        public string Name;
+        public string Value;
+    }
+
+    public class ConnectionInfo
+    {
+        public Guid ParentId;
+        public List<FlowInfo> Flow;
+    }
+
+    public class FlowInfo
+    {
+        public readonly string Product;
+        public readonly float MaxFlow;
+        public readonly float MinFlow;
+        public string UOM;
+    }
+
+
+    public class ProductionLimits
+    {
+        public string Product;
+        public float MinProduction;
+        public float MaxProduction;
+        public string UOM;
+    }
+}
